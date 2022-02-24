0f0f071357ba13048f1bcb494059e9504a06af6d
Hongbusi <q951686662@qq.com>
2022-02-24 17:54:23 +0800

    feat: component

---
+++ pages/component/component.vue
@@ -0,0 +1,49 @@
+<template>
+  <view class="component-demo">
+    <view class="title">button</view>
+    <button type="primary">button</button>
+  </view>
+
+  <view class="component-demo">
+    <view class="title">checkbox-group</view>
+    <checkbox-group>
+      <label>
+        <checkbox value="cb" checked="true" color="#FFCC33" style="transform:scale(0.7)" />选中
+      </label>
+      <label>
+        <checkbox value="cb" color="#FFCC33" style="transform:scale(0.7)" />未选中
+      </label>
+    </checkbox-group>
+  </view>
+
+  <view class="component-demo">
+    <view class="title">editor</view>
+    <editor id="editor" class="ql-container" placeholder="请输入你的内容"></editor>
+  </view>
+</template>
+
+<script>
+</script>
+
+<style>
+  .component-demo {
+    text-align: center;
+    padding-bottom: 20rpx;
+    border-bottom: 1rpx solid red;
+  }
+
+  .title {
+    margin: 4rpx 0;
+    font-size: 16px;
+  }
+
+  .container {
+    padding: 10px;
+  }
+
+  #editor {
+    width: 100%;
+    height: 100px;
+    background-color: #ccc;
+  }
+</style>
---
+++ pages/me/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <view>
+    about me
+  </view>
+</template>
